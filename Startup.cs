7db6f0cc91114f7c5e2f94470a8dd9afb3d184c1
Jackal Lee <jackcakal@gmail.com>
2019-01-11 18:33:49 +0800

    Set up connection strings using SqlServer

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace CoinStudyWeb
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
+                options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
